12d535f823053e0777df78587afa25fbe4a5294a
luolei <meleilei@gmail.com>
2014-05-03 16:08:35 +0800

    重新配置grunt，添加压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,41 @@
 module.exports=function(grunt){
-    require('time-grunt')(grunt);
-    //配置 Ghost 本地开发主题
+    require('time-grunt')(grunt);//Grunt处理任务进度条提示
+
     grunt.initConfig({
         //默认文件目录在这里
         paths:{
-            assets:'./assets',//输出的最终文件都在这里
-            scss:'./css/scss',
-            css:'./css'
+            assets:'./assets',//输出的最终文件assets里面
+            scss:'./css/sass',//推荐使用Sass
+            css:'./css', //若简单项目，可直接使用原生CSS，同样可以grunt watch:base进行监控
+            js:'./js', //js文件相关目录
+            img:'./img' //图片相关
         },
         buildType:'Build',
         pkg: grunt.file.readJSON('package.json'),
-        archive_name: grunt.option('name') || 'ghostwill',
+        archive_name: grunt.option('name') || 'StaticPage项目名称',//此处可根据自己的需求修改
 
         //清理掉开发时才需要的文件
         clean: {
-            pre: ['dist/', 'build/'],
-            post: ['<%= archive_name %>.zip']
+            pre: ['dist/', 'build/'],//删除掉先前的开发文件
+            post: ['<%= archive_name %>*.zip'] //先删除先前生成的压缩包
         },
 
-        //压缩最终的theme 文件
+        uglify:{
+            options:{
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' //js文件打上时间戳
+            },
+            dist: {
+                 files: {
+                     '<%= paths.assets %>/js/min.v.js': '<%= paths.js %>/custome.js'
+                 }
+            }
+        },
+
+        //压缩最终Build文件夹
         compress:{
             main:{
                 options:{
-                    archive:'<%= archive_name %>-<%= grunt.template.today("yyyy-mm-dd") %>.zip'
+                    archive:'<%= archive_name %>-<%= grunt.template.today("yyyy") %>年<%= grunt.template.today("mm") %>月<%= grunt.template.today("dd") %>日<%= grunt.template.today("h") %>时<%= grunt.template.today("TT") %>.zip'
                 },
                 expand:true,
                 cwd:'build/',
@@ -35,14 +48,21 @@ module.exports=function(grunt){
             main:{
                 files:[
                     {expand: true, src: ['assets/css/**'], dest: 'build/'},
-                    {expand: true, src: ['assets/fonts/**'], dest: 'build/'},
                     {expand: true, src: ['assets/images/**'], dest: 'build/'},
                     {expand: true, src: ['assets/js/**'], dest: 'build/'},
-                    {expand: true, src: ['partials/**'], dest: 'build/'},
-                    {expand: true, src: ['scss/**'], dest: 'build/'},
-                    {expand: true, src: ['*', '!.gitignore', '!.DS_Store'], dest: 'build/'},
+                    {expand: true, src: ['*', '!.gitignore', '!.DS_Store','!Gruntfile.js','!package.json','!node_modules/**','!go.sh','!.ftppass','!<%= archive_name %>*.zip'], dest: 'build/'},
                 ]
             },
+
+            images:{
+                        expand: true,
+                        cwd:'img/',
+                        src: ['**','!github.png'],
+                        dest: 'assets/images/',
+                        flatten:true,
+                        filter:'isFile',
+            },
+
 
             archive:{
                 files:[
@@ -55,7 +75,8 @@ module.exports=function(grunt){
         sass:{
             admin:{
                 options:{
-                    sourcemap:true
+                    sourcemap:true,
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
                 },
                 files:{
                     '<%= paths.css %>/style.css':'<%= paths.scss %>/style.scss',
@@ -63,8 +84,8 @@ module.exports=function(grunt){
             }
         },
 
+        //压缩 css
         cssmin:{
-            //压缩 css
             options:{
                   keepSpecialComments: 0
               },
@@ -77,7 +98,22 @@ module.exports=function(grunt){
               }
         },
 
-        //监听变化
+        // 格式化和清理html文件
+        htmlmin: {
+            dist: {
+                options: {
+                removeComments: true,
+                //collapseWhitespace: true //压缩html:根据情况开启与否
+            },
+
+            files: {
+                'build/index.html': 'build/index.html',//清除html中的注释
+                }
+            }
+        },
+
+
+        //监听变化 默认grunt watch 监测所有开发文件变化
         watch:{
             options:{
                 //开启 livereload
@@ -88,14 +124,39 @@ module.exports=function(grunt){
                     grunt.log.writeln('Wating for more changes...');
                 }
             },
-
             //css
             sass:{
                 files:'<%= paths.scss %>/**/*.scss',
                 tasks:['sass:admin','cssmin']
             },
-            //JavaScript
+            css:{
+                files:'<%= paths.css %>/**/*.css',
+                tasks:['cssmin']
+            },
+            js:{
+                 files:'<%= paths.js %>/**/*.js',
+                 tasks:['uglify']
+            },
+            //若不使用Sass，可通过grunt watch:base 只监测style.css和js文件
+            base:{
+                files:['<%= paths.css %>/**/*.css','<%= paths.js %>/**/*.js','img/**'],
+                tasks:['cssmin','uglify']
+            }
 
+        },
+
+        //发布到FTP服务器 : 请注意密码安全，ftp的帐号密码保存在主目录 .ftppass 文件
+        'ftp-deploy': {
+          build: {
+            auth: {
+              host: 'yourftp.domain.com',
+              port: 21,
+              authKey: 'key1'
+            },
+            src: 'build',
+            dest: '/home/ftp/demo',
+            exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db', 'path/to/dist/tmp']
+          }
         }
 
     });
@@ -111,11 +172,14 @@ module.exports=function(grunt){
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-
-    grunt.registerTask('default', ['sass:admin']);
-    //执行 grunt bundle --name 打包最终主题为 name.zip 文件
-    grunt.registerTask('bundle', ['clean:pre', 'copy:main','cssmin','copy:archive', 'clean:post','compress']);
-
+    grunt.registerTask('default', ['cssmin','uglify','htmlmin','copy:images']);
+    grunt.registerTask('sass', ['sass:admin','cssmin']);
+    //执行 grunt bundle --最终输出的文件 < name-生成日期.zip > 文件
+    grunt.registerTask('bundle', ['clean:pre','copy:images', 'copy:main','cssmin','copy:archive', 'clean:post','htmlmin','compress',]);
+    //执行 grunt publish 可以直接上传项目文件到指定服务器FTP目录
+    grunt.registerTask('publish', ['ftp-deploy']);
 
 };
